a4b5cd04d6c8b1b8f198466eb944252e99d907be
lukaszabramczuk <ld.abramczuk@gmail.com>
2017-11-23 14:31:20 +0100

    formating <td>

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import {
 
 
 import {connect} from 'react-redux'
-import {database} from "./firebase";
+// import {database} from "./firebase";
 
 import EditTask from './Components/EditTask/EditTask'
 import DeleteTaskCheckbox from './Components/DeleteTaskCheckbox/DeleteTaskCheckbox'
