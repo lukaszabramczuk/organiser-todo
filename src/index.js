996c91314974da6612d48bd35439d43857e8759c
lukaszabramczuk <ld.abramczuk@gmail.com>
2018-02-12 21:49:10 +0100

    naprawiony bootsrap

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { Provider } from 'react-redux'
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import App from './App'
 import store from './store'
